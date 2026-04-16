29f2b6836d78eeab18c3d2ab2ea56a85b52aefa9
Ryo Igarashi <48922999+Ryo1200219@users.noreply.github.com>
2026-04-17 02:04:45 +0900

    fix qty placeholder

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 // 【ここだけ書き換えればOK】
-const APP_VERSION = "v1.1.20";
+const APP_VERSION = "v1.1.21";
 const GAS_URL = "https://script.google.com/macros/s/AKfycbxE2WUH8SuUtiCf_Mkpa2rE4FNXdvR1vFc_q0VkCfTNx3jqG75aTPDPbHR21X9xMIFncA/exec";
 const CACHE_NAME = `inventory-${APP_VERSION}`;
